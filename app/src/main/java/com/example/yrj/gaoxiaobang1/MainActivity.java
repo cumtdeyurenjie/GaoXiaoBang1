a9543d78b93e41c7694c308227e4b8e4193d76ea
cumtdeyurenjie <1824075474@qq.com>
2018-03-15 15:02:18 +0800

    change

--- app/src/main/java/com/example/yrj/gaoxiaobang1/MainActivity.java
+++ app/src/main/java/com/example/yrj/gaoxiaobang1/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
        // initPcNews();
         final BottomNavigationView navigation =findViewById(R.id.navigation);
